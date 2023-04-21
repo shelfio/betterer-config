4ccfa6336231584479b59b505df9ed30aaf3a451
hmelenok <hmelenok@gmail.com>
2023-04-21 12:03:47 +0300

    feat: add esbuild with CJS

--- esbuild.js
+++ esbuild.js
@@ -4,4 +4,9 @@ import {common} from '@shelf/esbuild-config';
 await common({
   entryPoints: ['./index.js', './frontend.js'],
   target: ['node14'],
+  config: {
+    //betterer not supports imports in esbuild
+    format: 'cjs',
+    splitting: false,
+  },
 });
